7924ded21fd7e3914ecb79b0af63d43de72e6c41
dillonpw <dillonpwalsh10@gmail.com>
2023-08-13 18:57:13 -0400

    another attempt still not working

--- main.js
+++ main.js
@@ -1,50 +1,66 @@
+function submitForm() {
+    if (validateForm()) {
+        clearErrorMessages(); // Clear any previous error messages
+        document.getElementById('submitButton').disabled = true;
+        return true;  // Allow form submission
+    }
+    return false; // Prevent form submission
+}
+
 function validateForm() {
+    // Get form input elements
     const fname = document.getElementById('fname');
     const lname = document.getElementById('lname');
-    const emailInput = document.getElementById('email');
-    const signPass = document.getElementById('password');
-
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    
+    // Get error message elements
     const fnameError = document.getElementById('fnameError');
     const lnameError = document.getElementById('lnameError');
     const emailError = document.getElementById('emailError');
     const passwordError = document.getElementById('passwordError');
-
-    const isfNameValid = fname.value.trim() !== '';
-    const islNameValid = lname.value.trim() !== '';
-    const isEmailValid = emailInput.value.trim() !== '';
-    const isPasswordValid = signPass.value.trim() !== '';
-
-    fnameError.textContent = isfNameValid ? '' : 'First name is required.';
-    lnameError.textContent = islNameValid ? '' : 'Last name is required.';
-    emailError.textContent = isEmailValid ? '' : 'Email is required.';
-    passwordError.textContent = isPasswordValid ? '' : 'Password is required.';
-
-    const isFormValid =
-        isfNameValid && islNameValid && isEmailValid && isPasswordValid;
-
-    const submitButton = document.getElementById('submitButton');
-    submitButton.disabled = !isFormValid;
-
-    if (!isFormValid) {
-        alert('Please fill out all the required fields before submitting.');
+    
+    
+    // Check each field and display error messages if necessary
+    let isValid = true;
+    
+    if (fname.value === '') {
+        fnameError.textContent = 'Please enter your first name';
+        isValid = false;
     }
+    if (lname.value === '') {
+        lnameError.textContent = 'Please enter your last name';
+        isValid = false;
+    }
+    if (email.value === '') {
+        emailError.textContent = 'Please enter your email address';
+        isValid = false;
+    } else if (!validateEmail(email.value)) {
+        emailError.textContent = 'Please enter a valid email address';
+        isValid = false;
+    }
+    if (password.value === '') {
+        passwordError.textContent = 'Please enter a password';
+        isValid = false;
+    }
+    
+    // Form is valid if no errors are displayed
+    return isValid;  // Prevent form submission if there are errors
 }
 
-const formInputs = document.querySelectorAll('.myForm input');
-formInputs.forEach((input) => {
-    input.addEventListener('input', validateForm);
-});
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
 
-const myForm = document.querySelector('.myForm');
-myForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    validateForm();
-
-    const fname = document.getElementById('fname').value;
-    const lname = document.getElementById('lname').value;
-    const email = document.getElementById('email').value;
-
-    let message = `Name: ${fname} ${lname} \nEmail: ${email}\n is this correct?`;
-    alert(message);
-});
+function clearErrorMessages() {
+    const fnameError = document.getElementById('fnameError');
+    const lnameError = document.getElementById('lnameError');
+    const emailError = document.getElementById('emailError');
+    const passwordError = document.getElementById('passwordError');
+    
+    fnameError.textContent = '';
+    lnameError.textContent = '';
+    emailError.textContent = '';
+    passwordError.textContent = '';
+}
